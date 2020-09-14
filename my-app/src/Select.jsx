b50e8c121a39a66ddf6c228fe691e9b741b00c3d
IvanaDjuric <iwanadjrc@yahoo.com>
2020-09-14 12:15:41 +0200

    dodati fajlovi

--- my-app/src/Select.jsx
+++ my-app/src/Select.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { getAllCountries } from './services'
+import {getByRegion} from './services'
 
 
 const Select = ({setCountries,setShow}) => {
--- my-app/src/services.js
+++ my-app/src/services.js
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const getAllCountries = () => {
     return axios.get('https://restcountries.eu/rest/v2/all')
 }
+
+export const getByRegion = () => {
+    return axios.get('https://restcountries.eu/rest/v2/regionalbloc/eu')
+}
